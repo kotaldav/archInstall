062665aa97dd157226f79083dfbe71ec1b0881f2
David Kotalík <kotalik.dav@gmail.com>
2020-02-12 22:00:44 +0100

    Partitioning disk.

--- archInit.sh
+++ archInit.sh
@@ -24,3 +24,14 @@ devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
 device=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1
 clear
 
+## Calculating swap size
+## For possibility of using hibernation, swap size needs to be at least the size of RAM
+swap_size=$(free -m | awk '/Mem:/ {print $2')
+swap_end=$(( $swap_size + 1 + 250 ))
+
+## Partitioning disk
+parted --script "${device}" -- mklabel gpt \
+  mkpart EXP fat32 1Mib 250MiB \
+  set 1 boot on \
+  mkpart primary linux-swap 250Mib ${swap_end} \
+  mkpart primary ext4 ${swap_end} 100%
---
+++ prd.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo `sqrt 4`
