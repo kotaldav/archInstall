05483fdddbf819c460bd6dcb61700ca56e3cacde
David Kotalík <kotalik.dav@gmail.com>
2020-02-12 20:33:16 +0100

    Minor fixes

--- archInit.sh
+++ archInit.sh
@@ -5,16 +5,22 @@ set -uo pipefail
 # Script url: https://git.io/JvWGU
 
 hostname=$(dialog --stdout --inputbox "Enter hostname" 0 0) || exit 1
+clear
 : ${hostname:?"hostname cannot be empty"}
 
 username=$(dialog --stdout --inputbox "Enter username" 0 0) || exit 1
+clear
 : ${username:?"username cannot be empty"}
 
 pass=$(dialog --stdout --passwordbox "Enter password" 0 0) || exit 1
+clear
 : ${pass:?"password cannot be empty"}
 
 pass2=$(dialog --stdout --passwordbox "Confirm password" 0 0) || exit 1
+clear
 [[ "$pass" == "$pass2" ]] || ( echo "Password did not match"; exit 1 )
 
 devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
+device=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1
+clear
 
